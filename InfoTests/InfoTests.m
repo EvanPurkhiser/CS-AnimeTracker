41f8ce986ee7a0df57d3dbf3108d6f5e7c6dcd1a
collard <collard@98277486-ac76-4d9b-89a9-98605d28aefd>
2013-10-04 17:00:26 +0000

    git-svn-id: https://dev.cs.uakron.edu/svn/iOS/001/students/emp36/Projects/Project2/Info@435 98277486-ac76-4d9b-89a9-98605d28aefd

---
+++ InfoTests/InfoTests.m
@@ -0,0 +1,34 @@
+//
+//  InfoTests.m
+//  InfoTests
+//
+//  Created by Michael Collard on 10/4/13.
+//  Copyright (c) 2013 collard. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InfoTests : XCTestCase
+
+@end
+
+@implementation InfoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
