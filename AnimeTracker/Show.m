b01da5c9cd7f0713ba65b8f2c4665ce7e3f730c5
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-10-17 03:16:45 -0400

    Add object representation of 'Show' entity

---
+++ AnimeTracker/Show.m
@@ -0,0 +1,24 @@
+//
+//  Show.m
+//  AnimeTracker
+//
+//  Created by Evan Purkhiser on 10/17/13.
+//  Copyright (c) 2013 Evan Purkhiser. All rights reserved.
+//
+
+#import "Show.h"
+
+
+@implementation Show
+
+@dynamic episodesTotal;
+@dynamic episodesWatched;
+@dynamic name;
+@dynamic notes;
+@dynamic rating;
+@dynamic watchingFinishedOn;
+@dynamic watchingStartedOn;
+@dynamic airingStatus;
+@dynamic summary;
+
+@end
