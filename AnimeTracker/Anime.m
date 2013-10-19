13e056a6b2055079bc0779793ccda6959b60d280
emp36 <emp36@98277486-ac76-4d9b-89a9-98605d28aefd>
2013-10-19 18:02:24 +0000

    Move Anime model logic into a category
    This also setsup a MALLoader class that will handle logic for loading
    anime / anime lists from the MAL API
    
    git-svn-id: https://dev.cs.uakron.edu/svn/iOS/001/students/emp36/Projects/Project2/Info@811 98277486-ac76-4d9b-89a9-98605d28aefd

--- AnimeTracker/Anime.m
+++ AnimeTracker/Anime.m
@@ -2,7 +2,7 @@
 //  Anime.m
 //  AnimeTracker
 //
-//  Created by Evan Purkhiser on 10/17/13.
+//  Created by Evan Purkhiser on 10/18/13.
 //  Copyright (c) 2013 Evan Purkhiser. All rights reserved.
 //
 
@@ -14,35 +14,12 @@
 @dynamic airingStatus;
 @dynamic episodesTotal;
 @dynamic episodesWatched;
+@dynamic image;
 @dynamic name;
 @dynamic notes;
 @dynamic rating;
 @dynamic summary;
 @dynamic watchingFinishedOn;
 @dynamic watchingStartedOn;
-@dynamic image;
-
-- (void)setDataFromMAL:(NSDictionary *)anime
-{
-    self.episodesTotal = anime[@"episodes"];
-    self.episodesWatched = anime[@"watched_episodes"];
-    self.name = anime[@"title"];
-    self.rating = anime[@"score"];
-    self.airingStatus = anime[@"status"];
-
-    // Store image as a BLOB in the database. Yes I realize the implications
-    // of storing binary data into a database, and yes this is very rarely a
-    // good idea. But this is pretty _damn cool_ so why not.
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0),
-    ^{
-        NSData *image = [NSData dataWithContentsOfURL:[NSURL URLWithString:anime[@"image_url"]]];
-
-        dispatch_async(dispatch_get_main_queue(),
-        ^{
-            self.image = image;
-            [[self managedObjectContext] save:nil];
-        });
-    });
-}
 
 @end
