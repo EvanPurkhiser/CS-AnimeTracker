634beb5a1edc32a5640cffb02dadf607ea3db90e
emp36 <emp36@98277486-ac76-4d9b-89a9-98605d28aefd>
2013-10-19 18:01:53 +0000

    Add object representation of 'Show' entity
    git-svn-id: https://dev.cs.uakron.edu/svn/iOS/001/students/emp36/Projects/Project2/Info@795 98277486-ac76-4d9b-89a9-98605d28aefd

---
+++ AnimeTracker/Show.h
@@ -0,0 +1,25 @@
+//
+//  Show.h
+//  AnimeTracker
+//
+//  Created by Evan Purkhiser on 10/17/13.
+//  Copyright (c) 2013 Evan Purkhiser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Show : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * episodesTotal;
+@property (nonatomic, retain) NSNumber * episodesWatched;
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * notes;
+@property (nonatomic, retain) NSNumber * rating;
+@property (nonatomic, retain) NSDate * watchingFinishedOn;
+@property (nonatomic, retain) NSDate * watchingStartedOn;
+@property (nonatomic, retain) NSString * airingStatus;
+@property (nonatomic, retain) NSString * summary;
+
+@end
